791f275480f5116b0bedc2e27f2bd458ff13478b
Lance <ljpruitt123@gmail.com>
2024-05-16 17:25:32 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Navbar from './components/navbar/Navbar'
+import Hero from './components/hero/Hero'
+import Programs from './components/programs/Programs'
+import Title from './components/title/Title'
+import About from './components/about/About'
+import Campus from './components/campus/Campus'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/footer/Footer'
+import VideoPlayer from './components/videoPlayer/VideoPlayer'
+
+const App = () => {
+
+  const [playState,setPlayState] = useState(false);
+
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
+        <Programs/>
+        <About setPlayState={setPlayState}/>
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus/>
+        <Title subTitle='TESTIMONIALS' title='What Students Say' />
+        <Testimonials/>
+        <Title subTitle='Contact Us' title='Get in Touch' />
+        <Contact/>
+        <Footer/>
+
+      </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
+    </div>
+  )
+}
+
+export default App
